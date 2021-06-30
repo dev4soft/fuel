29176d5a0a35bce91b31ec71f530c163a561fd56
andrei <work4program@gmail.com>
2021-06-30 11:46:06 +0700

    завершение рефакторинга класса Login

--- src/Auth.php
+++ src/Auth.php
@@ -11,70 +11,32 @@ class Auth
         $this->container = $c;
     }
 
-    private function getLoginToken(&$login, &$token, &$cookieIsSet)
-    {
-        // проверяем наличие сохраненной cookies
-        $cookieIsSet = true;
-        $login = $this->container->cookie->getCookieValue($request, 'login');
-        $token = $this->container->cookie->getCookieValue($request, 'token');
-
-        if (($login && $token)) {
-            return true;
-        }
-
-        $cookieIsSet = false;
-        // если нет в куках, посмотрим в сессии
-        $login = $this->container->session->login;
-        $token = $this->container->session->token;
-
-        if (($login && $token)) {
-            return true;
-        }
-
-        return false;
-    }
-
     public function __invoke($request, $response, $next)
     {
-        // проверяем наличие сохраненной cookies
-        $CookieSet = true;
-        $login = $this->container->cookie->getCookieValue($request, 'login');
-        $token = $this->container->cookie->getCookieValue($request, 'token');
+        $validator = new \Fuel\Login($this->container);
 
-        if (!($login && $token)) {
-            $CookieSet = false;
-            // если нет в куках, посмотрим в сессии
-            $login = $this->container->session->login;
-            $token = $this->container->session->token;
+        // проверим наличие данных в cookies
+        if ($validator->CheckCookie($request)) {
+            
+            // обновим сессию и куки
+            $validator->setSession();
+            $response = $validator->setCookies($response);
+
+        // проверим данные в сессии
+        } elseif ($validator->CheckSession()) {
+            
+            // обновим только сессию
+            $validator->setSession();
+            
+        } else {
+            // данные не найдены
+            
+            return $response->withRedirect('/login');
         }
 
-        if ($login && $token) {
-            $validator = new \Fuel\Models\Login($this->container, $login);
+        // передаем дальше
+        $response = $next($request, $response);
 
-            if ($validator->CheckToken($token)) {
-                // данные в сессии обновляются всегда
-                $this->container->session->login = $login;
-                $this->container->session->token = $token;
-
-                if ($CookieSet) {
-                    // cookie обновляется только если они существовали
-                    $response = $this->container->cookie->addCookie($response, 'login', $login, '10');
-                    $response = $this->container->cookie->addCookie($response, 'token', $token, '10');
-                }
-
-                // передаем дальше
-                $response = $next($request, $response);
-
-                return $response;
-            }
-
-            $this->container->session->delete('login');
-            $this->container->session->delete('token');
-
-            $response = $this->container->cookie->deleteCookie($response, 'login');
-            $response = $this->container->cookie->deleteCookie($response, 'token');
-        }
-
-        return $response->withRedirect('/login');
+        return $response;
     }
 }
--- src/Controllers/Login.php
+++ src/Controllers/Login.php
@@ -25,19 +25,20 @@ Class Login
         $login = $data['login'];
         $pass = $data['pass'];
 
-        $validator = new \Fuel\Models\Login($this->container, $login);
+        $validator = new \Fuel\Login($this->container);
+
+        $validator->getUserInfo($login);
+
         //$validator->updatePass($pass);
         
         if ($validator->CheckPass($pass)) {
-            $token = password_hash($login . $validator->hash, PASSWORD_BCRYPT);
-            $this->container->session->login = $login;
-            $this->container->session->token = $token;
+            $validator->MakeToken();
+            $validator->setSession();
 
             $remember = isset($data['remember']) ? $data['remember'] : false;
             
             if ($remember) {
-                $response = $this->container->cookie->addCookie($response, 'login', $login, '10');
-                $response = $this->container->cookie->addCookie($response, 'token', $token, '10');
+                $response = $validator->setCookies($response);
             }
 
             return $response->withRedirect('/');
@@ -48,11 +49,7 @@ Class Login
 
     public function LogOut($request, $response)
     {
-        $this->container->session->delete('login');
-        $this->container->session->delete('token');
-
-        $response = $this->container->cookie->deleteCookie($response, 'login');
-        $response = $this->container->cookie->deleteCookie($response, 'token');
+        $response = (new \Fuel\Login($this->container))->LogOut($response);
 
         return $response->withRedirect('/login');
     }
